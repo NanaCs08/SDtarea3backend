6f8f0d03a62ca8247e50a9c4e7a884c579cbac66
NanaCs08 <82248322+NanaCs08@users.noreply.github.com>
2024-10-14 18:55:38 -0600

    nuevos cmbios

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const serverless = require('express-serverless-http');
+const serverless = require('serverless-http');
 const cors = require('cors');
 require('dotenv').config();
 
 const publisherRoutes = require('./routes/publisherRoutes');
-// const authorRoutes = require('./routes/authorRoutes');
-// const bookRoutes = require('./routes/bookRoutes');
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -16,8 +14,6 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Conectado a MongoDB Atlas'))
   .catch(err => console.error('Error de conexión:', err));
 
-app.use('/api/publishers', publisherRoutes);
-// app.use('/api/authors', authorRoutes);
-// app.use('/api/books', bookRoutes);
+app.use('/.netlify/functions/api', publisherRoutes);
 
 module.exports.handler = serverless(app);
